96b62fea3dd7fd552cd4f7d788bff28da10a3ea0
ad/hbarbaric1@wmar3674 <hrvoje.barbaric@t.ht.hr>
2021-02-04 23:10:51 +0100

    changed hook

--- src/hooks/use-fetchData.effect.js
+++ src/hooks/use-fetchData.effect.js
@@ -6,27 +6,24 @@ export const useFetchData = (url, setIsLoaded, setNumberOfPages) => {
   useEffect(() => {
     setIsLoaded(true);
     const ourRequest = Axios.CancelToken.source();
-    const loadData = async () => {
-      Axios.get(url, {
-        cancelToken: ourRequest.token,
+    Axios.get(url, {
+      cancelToken: ourRequest.token,
+    })
+      .then(function (response) {
+        // handle success
+        setData(response.data);
+        //set number of pages and is only used on home page
+        setNumberOfPages &&
+          setNumberOfPages(Math.ceil(response.data.count / 20));
+        setIsLoaded(false);
       })
-        .then(function (response) {
-          // handle success
-          setData(response.data);
-          //set number of pages and is only used on home page
-          setNumberOfPages &&
-            setNumberOfPages(Math.ceil(response.data.count / 20));
-          setIsLoaded(false);
-        })
-        .catch(function (thrown) {
-          if (Axios.isCancel(thrown)) {
-            console.log("Request canceled", thrown.message);
-          } else {
-            // handle error
-          }
-        });
-    };
-    loadData();
+      .catch(function (thrown) {
+        if (Axios.isCancel(thrown)) {
+          console.log("Request canceled", thrown.message);
+        } else {
+          // handle error
+        }
+      });
     return () => {
       ourRequest.cancel();
     };
